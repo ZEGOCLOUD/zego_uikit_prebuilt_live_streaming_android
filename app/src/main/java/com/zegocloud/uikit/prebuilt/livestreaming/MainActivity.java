b97bd234dbc772e0184568140b85e38c95ca8e3a
alan <alanliu@zego.im>
2022-09-09 18:43:44 +0800

    update

---
+++ app/src/main/java/com/zegocloud/uikit/prebuilt/livestreaming/MainActivity.java
@@ -0,0 +1,13 @@
+package com.zegocloud.uikit.prebuilt.livestreaming;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
